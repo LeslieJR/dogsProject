33c8a192f427bd76c15228d25f9eacfeb14f2801
LeslieJR <l.reyes.ubiqum@gmail.com>
2021-07-02 18:06:09 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+const port = 4000;
+const path = require('path');
+
+app.get('/', (req, res) =>{
+    const filePath = path.join(__dirname, 'src','views', 'home.html');
+    res.sendFile(filePath)
+})
+
+const publicPath = path.join('src', 'public');
+app.use(express.static(publicPath))
+
+app.listen(port);
