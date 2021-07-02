0080887dc65837736876d11c37c14316dbcb2e1a
siljp24 <silvia_juarez_24@hotmail.com>
2021-07-02 21:02:14 +0200

    validar

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,3 +1,13 @@
 function guarda_ficha () {
-    console.log("guardar")
+    const nombre = document.getElementById("nombre")
+    const edad = document.getElementById("edad")
+    const raza = document.getElementById("raza")
+    const btn =document.getElementById("btn-save")
+
+    if(nombre.value && edad.value && raza.value){
+        btn.removeAttribute("disabled")
+    }else{
+        btn.setAttribute("disabled", true)
+    }
+    console.log("holaa")
 }
